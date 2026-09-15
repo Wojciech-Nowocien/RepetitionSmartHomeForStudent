07a569bb625a9b6fd9fff30f7200e8f7b930ee56
Wojciech Nowocie┼ä <wojciech2008.nn@gmail.com>
2026-09-15 13:40:07 +0200

    Add Main class

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+//Utwórz obiekt: SmartHome home = new SmartHome();
+//Następnie utwórz co najmniej:
+//    2 × Lamp
+//    1 × AirConditioner
+//    1 × Television
+//    1 × RobotVacuum
+//Przykładowe dane dobierz samodzielnie.
+//Następnie dodaj wszystkie urządzenia do SmartHome.
+
+//Program powinien przetestować wszystkie przygotowane funkcjonalności.
+//A. Wyświetl wszystkie urządzenia: home.showAllDevices();
+//B. Włącz wszystkie urządzenia: home.turnOnAll();
+//C. Ponownie wyświetl urządzenia: sprawdź, czy zmienił się ich stan.
+//D. Przetestuj lampę: zwiększ i zmniejsz jej jasność.
+//E. Przetestuj klimatyzator: zwiększ i zmniejsz temperaturę.
+//F. Przetestuj telewizor: zmień kanał i głośność.
+//G. Przetestuj harmonogram: Ustaw godzinę dla: AirConditioner i RobotVacuum
+//H. Wyświetl urządzenia sterowalne: home.showControllableDevices();
+//I. Wyświetl urządzenia z harmonogramem: home.showSchedulableDevices();
+//J. Wyłącz wszystkie urządzenia: home.turnOffAll();
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
