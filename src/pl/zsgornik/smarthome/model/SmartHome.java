82e5cc9cfff1aee628acdbace70f7bf4c8e19cfb
Wojciech Nowocie┼ä <wojciech2008.nn@gmail.com>
2026-09-15 12:54:16 +0200

    Add SmartHome class

---
+++ src/pl/zsgornik/smarthome/model/SmartHome.java
@@ -0,0 +1,30 @@
+package pl.zsgornik.smarthome.model;
+
+import pl.zsgornik.smarthome.model.Device;
+
+import java.util.List;
+
+//Powinna posiadać kolekcję: ArrayList<Device> lub: List<Device>
+
+//Utwórz: addDevice(Device device)
+//Utwórz: removeDevice(Device device)
+
+//Wyświetlanie urządzeń
+//Utwórz: showAllDevices() - metoda powinna przejść po wszystkich urządzeniach i wywołać: device.showInfo() - (nie używaj tutaj instanceof).
+
+//Dodaj: turnOnAll(), turnOffAll()
+//Metody powinny przejść po kolekcji urządzeń.
+//Nie twórz osobnych metod: turnOnAllLamps(), turnOnAllTelevisions(), turnOnAllAirConditioners()
+//Wykorzystaj wspólny typ: Device
+
+//Utwórz: showControllableDevices()
+//Metoda powinna wyświetlić tylko urządzenia implementujące: Controllable
+
+//Utwórz: showSchedulableDevices()
+//Metoda powinna wyświetlić tylko urządzenia implementujące: Schedulable
+
+public class SmartHome {
+
+    // TODO
+
+}
