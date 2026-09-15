042257cdc8de27a341799903ac8e8c14164f2b0a
Wojciech Nowocie┼ä <wojciech2008.nn@gmail.com>
2026-09-15 13:14:37 +0200

    Finish SmartHome class

--- src/pl/zsgornik/smarthome/model/SmartHome.java
+++ src/pl/zsgornik/smarthome/model/SmartHome.java
@@ -1,7 +1,6 @@
 package pl.zsgornik.smarthome.model;
 
-import pl.zsgornik.smarthome.model.Device;
-
+import java.util.ArrayList;
 import java.util.List;
 
 //Powinna posiadać kolekcję: ArrayList<Device> lub: List<Device>
@@ -24,7 +23,83 @@ import java.util.List;
 //Metoda powinna wyświetlić tylko urządzenia implementujące: Schedulable
 
 public class SmartHome {
+    private List<Device> devices;
 
-    // TODO
+    public SmartHome() {
+        this.devices = new ArrayList<>();
+    }
 
+    public void addDevice(Device device) {
+        devices.add(device);
+        System.out.println("Dodano urządzenie: " + device.getName());
+    }
+
+    public void removeDevice(Device device) {
+        if (devices.remove(device)) {
+            System.out.println("Usunięto urządzenie: " + device.getName());
+        } else {
+            System.out.println("Nie znaleziono urządzenia.");
+        }
+    }
+
+    public void showAllDevices() {
+        System.out.println("Wszytskie urządzenia:");
+        devices.stream()
+                .forEach(Device::showInfo);
+        System.out.println("\n \n");
+    }
+
+    public void turnOnAll() {
+        System.out.println("Włączanie wszytskich urządzen.");
+        devices.stream()
+                .forEach(Device::turnOn);
+        System.out.println("Urządzenia włączone.");
+    }
+
+//    public void turnOfAll() {
+//        System.out.println("Wyłączanie wszytskich urządzen.");
+//        devices.stream()
+//                .forEach(Device::turnOff);
+//        System.out.println("Urządzenia wyłączone.");
+//    }
+
+    public void turnOffAll() {
+        System.out.println("Wyłączanie wszytskich urządzen.");
+        for (Device device : devices) {
+            device.turnOff();
+        }
+        System.out.println("Urządzenia wyłączone.");
+    }
+
+//    public void showControllableDevices() {
+//        System.out.println("Urządzenia sterowalne:");
+//        devices.stream()
+//                .filter(device -> device instanceof Controllable)
+//                .forEach(device -> System.out.println(device.getName()));
+//        System.out.println();
+//    }
+
+    public void showControllableDevices() {
+        System.out.println("Urządzenia sterowalne:");
+        for (Device device : devices) {
+            if (device instanceof Controllable) {
+                System.out.println(device.getName());
+            }
+        }
+        System.out.println();
+    }
+
+    public void showSchedulableDevices() {
+        System.out.println("Urządzenia z harmonogramem:");
+        for (Device device : devices) {
+            if (device instanceof Schedulable) {
+                System.out.println(device.getName());
+            }
+        }
+        System.out.println();
+    }
+
+    public List<Device> getDevices() {
+        return devices;
+    }
 }
